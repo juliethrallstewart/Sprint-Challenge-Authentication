17455a10a8bb69de127adfcaed1520bdc35f0d9c
Julie Thrall <juliethrallstewart@gmail.com>
2019-09-20 09:22:20 -0700

    register & login functionality completed in auth-router.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,52 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const Users = require('../users/users-model.js');
+const secrets = require('../config/secrets')
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12); // 2 ^ n
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      const token = generateToken(user)
+
+      res.status(201).json(token);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user)
+        res.status(200).json({token});
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
+function generateToken (user) {
+  const payload = {
+    username: user.username
+  }
+  const options = {
+    expiresIn: '1d'
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
 module.exports = router;
+
---
+++ config/secrets.js
@@ -0,0 +1,4 @@
+module.exports = {
+    jwtSecret: process.env.JWT_SECRET || 'the fears are paper tigers'
+
+}
